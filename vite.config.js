396dba8e9b0c676e3a4c2d0726d5457b0f39ea0c
RojanSr <rairojan1116@gmail.com>
2023-06-22 20:23:06 +0545

    handle mime type error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Generate-Color-Pallete/",
+  base: "/Generate-Color-Pallete-With-ChakraUI/",
 });
